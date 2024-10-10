af0cac3c65bfff4566531924fc2e96d89e7b8e14
Pavel <pal5555@mail.ru>
2024-10-10 17:14:29 +0300

    'validateFormLogin'

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,19 +1,50 @@
+import { useForm, SubmitHandler } from "react-hook-form"
+
 export const Login = ()=>{
+    const {
+        register,
+        formState:{errors},
+        handleSubmit
+
+    } = useForm()
+
+    const isLogin = (data)=>{
+
+        console.log(data.email, data.password)
+    }
     return(
         <div className="login">
-        <form action='form' className="login__form">
+        <form action='form' className="login__form" onSubmit={handleSubmit(isLogin)}>
                 <div className='form__wrapper'>
                 <div className="login__title">
                     <h1>Войти</h1>
                 </div>
                 <div className="login__email">
-                    <input type="text" className='login__input' placeholder='Вашь E-mail'/>
+                    <input type="text" className='login__input' placeholder='Вашь E-mail'
+                    {...register('email',{
+                        required:'Поле к заполнению',
+                        pattern:{
+                            value: /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i,
+                            message: 'Введите правельный Email'
+                        }
+                    })}
+                    />
+                    <div className = 'input__error'>{errors?.email?.message}</div>
                 </div>
                 <div className="login__password">
-                    <input type="text" className='login__input' placeholder='Вашь пароль'/>
+                    <input type="password" className='login__input' placeholder='Вашь пароль'
+                    {...register('password',{
+                        required:'Поле к заполнению',
+                        minLength:{
+                            value: 6,
+                            message: 'Пароль меньше 6 символов!'
+                        }
+                    })}
+                    />
+                    <div className='input__error'>{errors?.password?.message}</div>
                 </div>
                 <div className="form__button">
-                    <button   type='button' className="login__success">
+                    <button   type='Submit' className="login__success">
                         Войти
                     </button>
                 </div>
