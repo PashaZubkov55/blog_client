47ad8dd16d157d40a6abe6ea3de72b600cca6e9d
Pavel <pal5555@mail.ru>
2024-12-16 01:58:53 +0300

    'design_button'

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -2,6 +2,19 @@ import React from "react";
 
 export const SettingsPage = ()=>{
     return(
-        <>SETTINGS__PAGE</>
+        <div className="setting">
+            <div className="setting__wrapper">
+                <div className="setting__item">
+                <button className="setting__btn">
+                    Редактировать  данные
+                </button>
+                </div>
+                <div className="setting__item">
+                <button className="setting__btn">
+                    Добавить пост 
+                </button>
+                </div>
+            </div>
+        </div>
     )
 }
