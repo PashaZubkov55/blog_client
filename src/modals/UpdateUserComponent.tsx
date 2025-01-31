115f77629ac632a6234cc1267c78b5078b64a192
Pavel <pal5555@mail.ru>
2025-01-31 15:05:18 +0300

    'update_User_form_validate'

--- src/modals/UpdateUserComponent.tsx
+++ src/modals/UpdateUserComponent.tsx
@@ -1,18 +1,27 @@
 import React from "react"
 import { useDispatch } from "react-redux"
 import { setModal } from "../store/Auth/AuthSlice"
+import { useForm } from "react-hook-form"
 export const UpdateUserComponent =()=>{
 
     const dispatch = useDispatch()
     const modalClose= (status:any)=>{
         dispatch(setModal(status))
     }
+    const {
+        register,
+        formState:{errors},
+        handleSubmit,
+    } = useForm()
+   const updateUser = (data)=>{
+        console.log(data)
+    }
 
 
     return(
             <div className="modal">
                 <div className="modal__container">
-                    <form className="modal__body">
+                    <form className="modal__body" onSubmit={handleSubmit(updateUser)}>
                         <div className="modal__title">
                         <span className="modal__close" onClick={()=>{modalClose(false)}}></span>
                         </div>
@@ -23,18 +32,31 @@ export const UpdateUserComponent =()=>{
                             <input 
                             type="text" 
                             placeholder="Павел"
-                            className="input-text" />
+                            className="input-text" 
+                           {...register('text',{
+                            required: 'Поля обизательное'
+                           })}
+                            />
                         </div>
+                        <div className = 'input__error'>{errors?.text?.message}</div>
                         <img 
                         src="https://www.kindpng.com/picc/m/495-4952535_create-digital-profile-icon-blue-user-profile-icon.png" 
                         alt="avatar" 
                         className="modal__img"
                         />
                         <div className="modal__input">
-                        <input type="file" className="input--file" />
+                        <input 
+                        type="file" 
+                        className="input--file" 
+                         {...register('file')}
+                        
+                        />
                         </div>
                     <div className="modal__sucsess">
-                    <button type="button" className="button-success"> Создать</button>
+                    <button 
+                    type="submit" 
+                    className="button-success"
+                    > Создать</button>
                    </div>
                         
                     </form>
