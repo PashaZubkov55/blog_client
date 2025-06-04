5f22a1b0faa61c597c4ca9dd8a27ff265a5bbfbc
Pavel <pal5555@mail.ru>
2025-06-04 16:15:50 +0300

    'post_detail-new_design'

--- src/components/Postdetail.tsx
+++ src/components/Postdetail.tsx
@@ -23,21 +23,22 @@ export const Postdetail = ()=>{
         }
         return(
         
-        <div className=" flex justify-center ">    
-        <div className="detail  px-5 py-4 ">
+        <div className=" flex justify-center   ">    
+        <div className="detail bg-white  overflow-hidden  max-w-200 mb-18 pb-6 mt-8 bg-white border border-gray-200 rounded-lg shadow-sm    ">
         <div className="detail__header ">
-        <img className= 'object-cover  bg-center h-100 w-400' src="https://images.unsplash.com/photo-1454496522488-7a8e488e8606" alt="detail"/>
+        <img className= 'object-cover  bg-center h-100 w-400 ' src="https://images.unsplash.com/photo-1454496522488-7a8e488e8606" alt="detail"/>
           </div>      
-                <div className="detail__title ">
-                    <h1 className='title   text-5xl font-extrabold dark:text-white '>{data.title}</h1>
+                <div className="detail__title px-8 ">
+                    <h1 className='title   text-3xl font-extrabold dark:text-white px8 '>{data.title}</h1>
                     </div>
-                <div className="detail__description text-xl py-5">{data.body}</div>
-                <div className="detail_buttons ">
+                <div className="detail__description text-lg  py-5 px-8">{data.body}</div>
+                <div className="detail_buttons px-8">
                 <div className="details_button">
                 <button type='button' className="mb-2 block   bg-green-700 w-full rounded  text-white px-6 pb-2 pt-2.5 text-xs hover:bg-green-800  font-medium uppercase leading-normal  shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong ">Редактировать</button>
                 </div>
-                
+                <div className="details_button">
                 <button type="button" className="mb-2 block   bg-red-700 w-full rounded  text-white px-6 pb-2 pt-2.5 text-xs hover:bg-red-800  font-medium uppercase leading-normal  shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">Удалить</button>
+                </div>
                 </div>
 
      </div>
