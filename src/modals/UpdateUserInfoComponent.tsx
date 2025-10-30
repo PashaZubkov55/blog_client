c095414799ffd6af82d823df37711fe2f75d3e86
Pavel <pal5555@mail.ru>
2025-10-30 17:08:17 +0300

    'update_userInfoComponent-design'

---
+++ src/modals/UpdateUserInfoComponent.tsx
@@ -0,0 +1,107 @@
+import { useEffect, useState } from "react"
+import { useForm } from "react-hook-form"
+import { setModal } from "../store/Auth/AuthSlice"
+import { useDispatch } from "react-redux"
+import { URL_SERVER } from "../router/Url"
+export const UpdateUserInfoComponent =({name, img})=>{
+    const dispatch = useDispatch()
+    const [imageVisible, setImageVisible] = useState('')
+    const {
+        register,
+        formState:{errors},
+        setValue,
+        handleSubmit
+        
+
+    } = useForm()
+    const modalClose = ( status:any)=>{
+   
+        dispatch(setModal(status))
+        
+        }
+    useEffect(()=>{
+        setValue('name', name)
+        setValue('img', img)
+
+    },[])
+    const handleFileChange =(event)=>{
+      console.log('выбранный файл',event.target.files[0].name)
+      setImageVisible(URL.createObjectURL(event.target.files[0]))
+    }
+
+const updateUserInfo =()=>{
+  
+
+}
+    return(
+        <div className="modal"  onClick={(e)=>{modalClose( false)}}>
+        <div  className=" overflow-y-auto bg-green-50    overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center  items-center h-full w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div className="modal__shawow  relative p-4 w-full max-w-2xl max-h-full">
+          <div className="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
+         
+                  
+                  <form className="modal_body relative px-20 py-5 " enctype="multipart/form-data" onSubmit={handleSubmit(updateUserInfo)}  onClick={(e)=>{e.stopPropagation()}}>
+                  <svg className="w-3 h-3 absolute right-5 top-5"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14"  onClick={()=>{modalClose(false)}} >
+                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                      </svg>
+                     <div className=" text-lg font-semibold modal__title flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
+                      Изменить данные
+                     </div>
+                     
+                     <div className="modal__input mt-2">
+                     <input 
+                     type="text"  id="text" 
+                     
+                     {...register('name',{
+                      required: 'Поля обизательное'
+                     })}
+                     onChange={(e)=>setValue('name', e.target.value)}
+                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder={name}  />
+                     </div>
+                     <div className = 'input__error mt-2 text-red-600'>{errors?.name?.message}</div>
+                     
+                    
+                     
+                     <div className=" person__awatar flex justify-center">
+                      {imageVisible? 
+                       <div className=" h-auto max-w-lg  my-3  ">
+                      <img className=" w-40 h-40 rounded-full border-3 border-green-400" src={imageVisible} alt='Woman looking front'/>
+                      </div>
+                      :
+                      <div className=" h-auto max-w-lg  my-3  ">
+                      <img className=" w-40 h-40 rounded-full border-3 border-green-400" src={URL_SERVER+img} alt='Woman looking front'/>
+                      </div>
+                        }
+                       
+                      </div>
+                     <div className="modal__files ">
+                     <input type="file" 
+                   {...register('file',{
+                    required:'файл не выбран'
+                   })}
+                     className=" modal__file  flex flex-column mt-4 text-white inline-flex w-full justify-center bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 "
+                    
+                     
+                    onChange={handleFileChange}
+                    />
+                      
+                      
+                      <div className = 'input__error mt-2 text-red-600'>{errors?.file?.message}</div>
+                      
+                     </div>
+                    
+                     <div className="modal__sucsess mt-2">
+                     <button  type="submit"  className="text-white inline-flex w-full justify-center bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                     Изменить пост
+                  </button>
+                     </div>
+                    
+                    
+                   
+                  </form>
+                  </div>
+                  </div>
+                  </div>
+                  </div>
+    )
+}
--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -2,14 +2,16 @@ import React, { useEffect } from "react";
 import { CreatePostComponent } from "../modals/CreatePostComponent";
 import { useDispatch, useSelector } from "react-redux";
 import { setModal } from "../store/Auth/AuthSlice";
-import { UpdateUserComponent } from "../modals/UpdateUserComponent";
 import { CreateUserInfo } from "../modals/CreateUserInfoComponent";
-
+import { useGetInfoQuery } from "../store/userInfo/userInfoApi";
+import { UpdateUserInfoComponent } from "../modals/UpdateUserInfoComponent";
 export const SettingsPage = ()=>{
     const modalStatus = useSelector((state)=> state.auth.modal)
     const dispatch = useDispatch()
-
+    const id = localStorage.getItem('userId')
+    const {data= []} = useGetInfoQuery(id)
     useEffect(()=>{
+        console.log('data-',data)
         console.log(modalStatus)
     },[modalStatus])
     const visibleModal =(status:any)=>{
@@ -31,12 +33,12 @@ export const SettingsPage = ()=>{
                 <div className="setting__item">
                 <div className="details_button mt-20">
                 <button 
-                 onClick={()=>{visibleModal('reateUnfo')}}
+                 onClick={()=>{visibleModal('createUnfo')}}
                 type='button'   className=" cursor-pointer mb-2 block   bg-green-700 w-full rounded  text-white px-6 pb-2 pt-2.5 text-xs hover:bg-green-800  font-medium uppercase leading-normal  shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong ">
                Добавить о себе</button>
                
                 <button 
-                 onClick={()=>{visibleModal('user')}}
+                 onClick={()=>{visibleModal('updateInfo')}}
                 type='button'   className=" cursor-pointer mb-2 block   bg-green-700 w-full rounded  text-white px-6 pb-2 pt-2.5 text-xs hover:bg-green-800  font-medium uppercase leading-normal  shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong ">
                Редактировать Профиль</button>
                
@@ -56,7 +58,13 @@ export const SettingsPage = ()=>{
                 
                 </div>
                { modalStatus.payload == 'post'?
-           <CreatePostComponent/> :modalStatus.payload == 'reateUnfo'? <CreateUserInfo/> :false
+            <CreatePostComponent/> 
+            :modalStatus.payload == 'createUnfo'? <CreateUserInfo/>
+            : modalStatus.payload == 'updateInfo'? <UpdateUserInfoComponent
+                name = {data.name}
+                img = {data.img}
+
+            />:false
             }
             </div>
         
