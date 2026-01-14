d1fa4f2faf302ae416e1d5006f9f8a3a7c990009
Pavel <pal5555@mail.ru>
2026-01-14 18:54:24 +0300

    'update_img_post_and_update_img_userInfo'

--- src/modals/UpdatePostComponent.tsx
+++ src/modals/UpdatePostComponent.tsx
@@ -141,7 +141,7 @@ export const UpdatePostComponent = ({title, description,  img})=>{
                      <div className="modal__files ">
                      <input type="file" 
                    {...register('file',{
-                    required:'файл не выбран'
+                    required: false
                    })}
                      className=" modal__file  flex flex-column mt-4 text-white inline-flex w-full justify-center bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 "
                     
@@ -150,7 +150,6 @@ export const UpdatePostComponent = ({title, description,  img})=>{
                     />
                       
                       
-                      <div className = 'input__error mt-2 text-red-600'>{errors?.file?.message}</div>
                       
                      </div>
                     
--- src/modals/UpdateUserInfoComponent.tsx
+++ src/modals/UpdateUserInfoComponent.tsx
@@ -42,7 +42,11 @@ const updateUserInfo = async(data)=>{
     try {
       const formData = new FormData()
       formData.append('name', data.name)
-      formData.append('img', data.file[0])
+      if (data.file) {
+        formData.append('img', data.file[0])
+        console.log('файл', data)
+      }
+      formData.append('img', '')
       formData.append('userId',userId)
       console.log('userId -',userId)
       await updateInfo({userId, body:formData}).unwrap()
@@ -105,7 +109,7 @@ const updateUserInfo = async(data)=>{
                      <div className="modal__files ">
                      <input type="file"
                    {...register('file',{
-                    required:'файл не выбран'
+                    required:false
                    })}
                      className=" modal__file  flex flex-column mt-4 text-white inline-flex w-full justify-center bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 "
                     
